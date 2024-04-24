9487f05b49269f5814fcbecd61f8b430cd7fccca
Somar Kesen <kesen.somar.99@gmail.com>
2024-04-24 15:01:02 +0300

    Integrate dotenv for secure API key management
    - Added `.env` to `.gitignore` to prevent committing sensitive environment variables.
    - Updated `main.dart` to load environment variables using `flutter_dotenv` before app initialization.
    - Refactored `WeatherService` to retrieve the API key from environment variables, removing the need to pass it through the constructor.
    - Added `flutter_dotenv` to dependencies in `pubspec.yaml` and updated `pubspec.lock` with the new dependency version.
    - Ensured consistent formatting and error handling improvements in `WeatherService`.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:minimal_weather_app/pages/weather_page.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+Future<void> main() async {
+  await dotenv.load(
+      fileName: ".env"); // Ensure dotenv is loaded before the app starts
   runApp(const MyApp());
 }
 
--- lib/services/weather_service.dart
+++ lib/services/weather_service.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:geocoding/geocoding.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:minimal_weather_app/models/weather_model.dart';
@@ -6,28 +7,30 @@ import 'package:http/http.dart' as http;
 
 class WeatherService {
   static const BASE_URL = "http://api.openweathermap.org/data/2.5/weather";
-  final String apiKey;
+  final String apiKey = dotenv.env['WEATHER_API_KEY'] ??
+      "default_api_key"; // Provide a default or handle null appropriately
 
-  WeatherService(this.apiKey);
+  WeatherService();
 
   Future<Weather> getWeather(String cityName) async {
-    final response = await http.get(Uri.parse('$BASE_URL?q=$cityName&appid=$apiKey&units=metric'));
+    final response = await http
+        .get(Uri.parse('$BASE_URL?q=$cityName&appid=$apiKey&units=metric'));
 
-    if(response.statusCode == 200){
+    if (response.statusCode == 200) {
       return Weather.formJson(jsonDecode(response.body));
-    }else{
+    } else {
       throw Exception('Failed to load weather Data');
     }
   }
 
   Future<String> getCurrentCity() async {
     LocationPermission permission = await Geolocator.checkPermission();
-    if(permission == LocationPermission.denied){
+    if (permission == LocationPermission.denied) {
       permission = await Geolocator.requestPermission();
     }
 
     Position position = await Geolocator.getCurrentPosition(
-      desiredAccuracy: LocationAccuracy.high);
+        desiredAccuracy: LocationAccuracy.high);
 
     List<Placemark> placemarks =
         await placemarkFromCoordinates(position.latitude, position.longitude);

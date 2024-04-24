78d7bf133df8f529007ab86685edb8b8e161c531
Somar Kesen <kesen.somar.99@gmail.com>
2024-04-24 14:40:35 +0300

    Enhance weather display with Lottie animations and update assets management
    - Import Lottie package in WeatherPage for richer visual representation.
    - Implement getWeatherAnimation function to select appropriate Lottie animation based on the weather condition.
    - Update UI to include Lottie animations and display the current weather condition text.
    - Adjust background color to grey[800] for better contrast with Lottie animations.
    - Correct assets section in pubspec.yaml to properly include asset files, ensuring that Lottie animations are correctly loaded.

--- lib/pages/weather_page.dart
+++ lib/pages/weather_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 import 'package:minimal_weather_app/models/weather_model.dart';
 import 'package:minimal_weather_app/services/weather_service.dart';
 
@@ -26,6 +27,31 @@ class _WeatherPageState extends State<WeatherPage> {
     }
   }
 
+  String getWeatherAnimation(String? mainCondition){
+    if (mainCondition == null) return 'assets/sunny.json';
+
+    switch (mainCondition.toLowerCase()){
+      case 'clouds':
+      case 'mist':
+      case 'smoke':
+      case 'haze':
+      case 'dust':
+      case 'fog':
+        return 'assets/cloud.json';
+      case 'rain':
+      case 'drizzle':
+      case 'shower rain':
+        return 'assets/rain.json';
+      case 'thunderstorm':
+        return 'assets/thunder.json';
+      case 'clear':
+        return 'assets/sunny.json';
+      default:
+        return 'assets/sunny.json';
+    }
+  }
+
+
   @override
   void initState() {
     // TODO: implement initState
@@ -38,14 +64,17 @@ class _WeatherPageState extends State<WeatherPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[800],
       body: Center(
         child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Text(_weather?.cityName ?? "Loading City ...."),
+          Lottie.asset(getWeatherAnimation(_weather?.mainCondition)),
           Text('${_weather?.temperature.round()}℃'),
+          Text(_weather?.mainCondition ?? ""),
     ],),
-      ),
+       ),
     );
   }
 }
